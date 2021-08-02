e69298f83313e2d0941112449a91bbf1c97207da
Ismail-Aslan <81377400+Ismail-Aslan@users.noreply.github.com>
2021-08-02 22:16:09 +0300

    lesson2 classComponent

---
+++ src/components/ClassComponent.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+class ClassComponent extends React.Component {
+    render() {
+        return(
+            //herşey tek bir div içerisinde olmak zorunda
+            <div className="wrapper"> 
+                <h1>
+                    Ismail ASLAN
+                </h1>
+            </div>
+        );
+    }
+}
+export default ClassComponent;
